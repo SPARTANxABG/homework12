ef8756fa90818518f3140ba940c56b84485dc612
Andres Olivares <spartan_abg@hotmail.com>
2024-12-07 13:56:02 -0600

    Changed path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meal-search/',
+  base: '/homework12/',
   plugins: [vue()],
   resolve: {
     alias: {
